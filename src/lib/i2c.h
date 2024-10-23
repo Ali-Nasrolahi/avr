71160b61beeaa13a789d09e0e264b723e1e97d33
Ali Nasrolahi <A.Nasrolahi01@gmail.com>
2024-10-23 18:25:10 +0330

    Minor change

--- src/lib/i2c.h
+++ src/lib/i2c.h
@@ -2,17 +2,6 @@
 
 #include "common.h"
 
-#define I2C_DDR  (DDRC)
-#define I2C_PORT (PORTC)
-
-#ifdef __AVR_ATmega328P__
-#define I2C_SCL (PINC5)
-#define I2C_SDA (PINC4)
-#elif defined(__AVR_ATmega32A__)
-#define I2C_SCL (PINC0)
-#define I2C_SDA (PINC1)
-#endif
-
 void i2c_init_master();
 void i2c_init_slave(uint8_t addr, void (*recv_f)(uint8_t), uint8_t (*req_f)(void));
 
